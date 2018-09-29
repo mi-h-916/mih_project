b5d4754d72dccaf351cefc8278d59998792fd9b6
mi-h <>
2018-09-29 23:32:55 +0900

    DataBindingが使用出来るように

--- app/src/main/java/com/example/mi_h916/mi_h_project/MainActivity.kt
+++ app/src/main/java/com/example/mi_h916/mi_h_project/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.mi_h916.mi_h_project
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mi_h916.mi_h_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    val binding:ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }

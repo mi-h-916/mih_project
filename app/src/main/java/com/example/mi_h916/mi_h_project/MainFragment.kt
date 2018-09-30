018e7403205adb873188836a950ada17dfb63c27
mi-h <>
2018-09-30 16:38:02 +0900

    RecyclerView（ver LinearLayout）を実装

--- app/src/main/java/com/example/mi_h916/mi_h_project/MainFragment.kt
+++ app/src/main/java/com/example/mi_h916/mi_h_project/MainFragment.kt
@@ -3,6 +3,8 @@ package com.example.mi_h916.mi_h_project
 import android.support.v4.app.Fragment
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +13,12 @@ import com.example.mi_h916.mi_h_project.databinding.FragmentMainBinding
 class MainFragment :Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = DataBindingUtil.inflate<FragmentMainBinding>(inflater,R.layout.fragment_main,container,false)
+        val binding = DataBindingUtil.inflate<FragmentMainBinding>(inflater,R.layout.fragment_main,container,false).apply {
+            val list = mutableListOf<String>("a","b","c","d")
+            recyclerView.layoutManager = LinearLayoutManager(activity)
+            recyclerView.adapter = RecyclerAdapter(list)
+            recyclerView.addItemDecoration(DividerItemDecoration(activity,DividerItemDecoration.VERTICAL))
+        }
         return binding.root
     }
 }
---
+++ app/src/main/java/com/example/mi_h916/mi_h_project/RecyclerAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.mi_h916.mi_h_project
+
+import android.databinding.DataBindingUtil
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mi_h916.mi_h_project.databinding.FragmentRecyclerItemBinding
+
+class RecyclerAdapter(val data: MutableList<String>): RecyclerView.Adapter<RecyclerAdapter.ItemHolder>(){
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder:ItemHolder, position: Int) {
+        holder.binding.text.text = data[position]
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ItemHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.fragment_recycler_item,parent,false)
+        return ItemHolder(v)
+    }
+
+    class ItemHolder(v: View):RecyclerView.ViewHolder(v) {
+        val binding = DataBindingUtil.bind<FragmentRecyclerItemBinding>(v)!!
+    }
+}

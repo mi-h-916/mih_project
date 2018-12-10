e0f58d31897ef5eb7db503b876cb175e6c3c849b
mi-h <>
2018-12-10 23:40:00 +0900

    seekbarの作製

--- app/src/main/java/com/example/mi_h916/mi_h_project/MainFragment.kt
+++ app/src/main/java/com/example/mi_h916/mi_h_project/MainFragment.kt
@@ -6,12 +6,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import com.example.mi_h916.mi_h_project.databinding.FragmentMainBinding
 
 class MainFragment :Fragment(){
 
+    private val list = listOf(5,10,15,20,25,30)
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = DataBindingUtil.inflate<FragmentMainBinding>(inflater,R.layout.fragment_main,container,false)
+        val binding = DataBindingUtil.inflate<FragmentMainBinding>(inflater,R.layout.fragment_main,container,false).apply {
+            text.setText(seekbar.progress.toString())
+
+            seekbar.setOnSeekBarChangeListener(
+                    object :SeekBar.OnSeekBarChangeListener {
+                        override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                            //ツマミをドラッグ
+                            seekBar?.let {
+                                val index = it.progress / list.size
+                                text.setText(list.get(index).toString())
+                            }
+                        }
+
+                        override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                            //ツマミを触れたとき
+                        }
+
+                        override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                            //ツマミを離した時
+                        }
+                    }
+            )
+        }
         return binding.root
     }
 }

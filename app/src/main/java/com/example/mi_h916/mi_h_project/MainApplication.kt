af4e3fad5bc04bbe4a4912eed864bf49a951f248
mi-h <>
2020-10-20 23:34:20 +0900

    application start

---
+++ app/src/main/java/com/example/mi_h916/mi_h_project/MainApplication.kt
@@ -0,0 +1,9 @@
+package com.example.mi_h916.mi_h_project
+
+import android.app.Application
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
